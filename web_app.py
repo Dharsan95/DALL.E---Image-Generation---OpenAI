3bbd192125a52b05055907f8088209baddd5f4d3
Dharsan95 <138092527+Dharsan95@users.noreply.github.com>
2024-03-15 12:43:32 +0530

    Add files via upload

---
+++ web_app.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+@author: admin
+"""
+
+import openai
+import urllib.request
+from PIL import Image
+import streamlit as st
+
+
+
+from openai import OpenAI
+client = OpenAI(api_key = "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")
+
+def generate_image(image_description):
+
+  img_response = client.images.generate(
+  model="dall-e-2",
+  prompt= image_description,
+  quality="standard",
+  n=1,
+)
+  
+
+  img_url = img_response.data[0].url
+
+  urllib.request.urlretrieve(img_url, 'img.png')
+
+  img = Image.open("img.png")
+  
+  return img
+
+
+
+# page title
+st.title('DALL.E - Image Generation - OpenAI')
+
+# text input box for image recognition
+img_description = st.text_input('Image Desription')
+
+if st.button('Generate'):
+    
+    generated_img = generate_image(img_description)
+    st.image(generated_img)
+    
